deb215e416e54938e9adaf23829b82fd09a57157
jb <jessb.epicodus@gmail.com>
2022-02-28 10:56:21 -0800

    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/hello")] // The route decorator is overriding the default URL path
+    public string Hello() { return "Hello friend!"; }
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+    [Route("/")]  // root path
+    public ActionResult Letter() 
+    { 
+      // model binding
+      LetterVariable myLetterVariable = new LetterVariable(); 
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      myLetterVariable.Location = "Iceland";
+      myLetterVariable.Souvenir = "Polar-Bear claw necklace";
+      return View(myLetterVariable); 
+    } 
+    [Route("/form")]
+    public ActionResult Form() { return View(); 
+    }
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      myLetterVariable.Location = location;
+      myLetterVariable.Souvenir = souvenir;
+      return View(myLetterVariable);
+    }
+  }
+}
